00fc575485a7118823092e972382ddca5bf141f7
Elman Astanov <e.astanov@asan.local>
2021-10-08 17:31:47 +0400

    uncontrolled to controlled solved. and debouncing was added

--- pages/search-client.tsx
+++ pages/search-client.tsx
@@ -1,28 +1,47 @@
-import {useState, useEffect} from 'react';
-import {useRouter} from 'next/router';
-import {SearchResult} from '../types/types';
-import {performSearch} from '../api/search';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useDebounce } from 'use-debounce';
+import { SearchResult } from "../types/types";
+import { performSearch } from "../api/search";
 
 export default function Search() {
   const router = useRouter();
-  const searchQuery = router.query.search; 
-  const [searchResults, setSearchResults] = useState<SearchResult[]>([])
+  const [query, setQuery] = useState("");
+  const [value] = useDebounce(query, 250);
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
+  const searchQuery = router.query.search;
   useEffect(() => {
-    if(!searchQuery){
+    if (!searchQuery) {
       return;
     }
-    performSearch(searchQuery as string).then(result => setSearchResults(result.data??[]))
-    .catch(error => {
-      alert("Opps!!! Something went wrong.");
-    })
+    setQuery(searchQuery as string);
+  }, [searchQuery]);
 
-  }, [searchQuery])
+  useEffect(() => {
+    if (value) {
+      performSearch(value)
+        .then((result) => setSearchResults(result.data ?? []))
+        .catch((error) => {
+          alert("Opps!!! Something went wrong.");
+        });
+    }
+  }, [value]);
 
   return (
     <div className="p-4">
-      <label htmlFor="searchQuery" className="mr-4">Search for:</label>
-      <input id="searchQuery" value={searchQuery} type="text" className="border" />
+      <label htmlFor="searchQuery" className="mr-4">
+        Search for:
+      </label>
+      <input
+        id="searchQuery"
+        value={query}
+        onChange={(e: React.FormEvent<HTMLInputElement>) => {
+          setQuery(e.currentTarget.value);
+        }}
+        type="text"
+        className="border"
+      />
       <ul>
         {searchResults.map(({ id, product: { title } }) => (
           <li key={id}>{title}</li>
