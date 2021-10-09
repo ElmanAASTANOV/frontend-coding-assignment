85125dd29c2042c62ca528d8303cbd4dd34fea23
ELMANASTANOV <elmanastanov@gmail.com>
2021-10-09 09:17:44 +0400

    use callback was moved to outside. I created new high order function and call it on onInput

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -20,13 +20,15 @@ export default function Profile() {
     console.log('Pretending to send form data...', Object.fromEntries(new FormData(e.currentTarget).entries()));
   };
 
+  const onInputCallBack = (actionName: string) => useCallback(data => dispatch({action: actionName, payload: data}), [])
+
   return (
     <form className="max-w-screen-sm mx-auto m-6 p-3 space-y-3 border" onSubmit={submitForm}>
-      <FormInput id="name" label="Name" onInput={useCallback(data => dispatch({action: "name", payload: data}), [])} errorMessage={getErrorMessage(state.name)} />
-      <FormInput id="email" label="Email" onInput={useCallback(data => dispatch({action: "email", payload: data}), [])} errorMessage={getErrorMessage(state.email)} />
-      <FormInput id="address" label="Address" onInput={useCallback(data => dispatch({action: "address", payload: data}), [])} errorMessage={getErrorMessage(state.address)} />
-      <FormInput id="country" label="Country" onInput={useCallback(data => dispatch({action: "country", payload: data}), [])} errorMessage={getErrorMessage(state.country)} />
-      <FormInput id="bio" label="About me" onInput={useCallback(data => dispatch({action: "bio", payload: data}), [])} errorMessage={getErrorMessage(state.bio)} />
+      <FormInput id="name" label="Name" onInput={onInputCallBack("name")} errorMessage={getErrorMessage(state.name)} />
+      <FormInput id="email" label="Email" onInput={onInputCallBack("email")} errorMessage={getErrorMessage(state.email)} />
+      <FormInput id="address" label="Address" onInput={onInputCallBack("address")} errorMessage={getErrorMessage(state.address)} />
+      <FormInput id="country" label="Country" onInput={onInputCallBack("country")} errorMessage={getErrorMessage(state.country)} />
+      <FormInput id="bio" label="About me" onInput={onInputCallBack("bio")} errorMessage={getErrorMessage(state.bio)} />
       <div className="text-center pt-4">
         <button type="submit" className="ml-auto border p-3">Save</button>
       </div>
