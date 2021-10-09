ac881df8e6f06b15c7cda1dfb88a1241d0a3ad71
ELMANASTANOV <elmanastanov@gmail.com>
2021-10-09 09:09:57 +0400

    on input enter, you can search with new value

--- pages/search-server.tsx
+++ pages/search-server.tsx
@@ -1,12 +1,19 @@
-import {SearchResult} from '../types/types'
-import {performSearch} from '../api/search'
-export default function Search({searchQuery, searchResults, errorMessage}) {
-
+import React, { KeyboardEventHandler } from 'react';
+import { SearchResult } from '../types/types'
+import { performSearch } from '../api/search'
+import { useRouter } from 'next/router'
+export default function Search({ searchQuery, searchResults, errorMessage }) {
+  const router = useRouter()
   return (
     <div className="p-4">
       {errorMessage && <h2>{errorMessage}</h2>}
       <label htmlFor="searchQuery" className="mr-4">Search for:</label>
-      <input defaultValue={searchQuery} id="searchQuery" type="text" className="border" />
+      <input defaultValue={searchQuery} id="searchQuery" type="text" onKeyUp={(event: React.KeyboardEvent) => {
+        if (event.key === "Enter") {
+          let query = (event.target as HTMLInputElement).value;
+          router.push(`?search=${query}`)
+        }
+      }} className="border" />
       <ul>
         {searchResults.map(({ id, product: { title } }) => (
           <li key={id}>{title}</li>
@@ -20,15 +27,15 @@ export async function getServerSideProps(context) {
   const searchQuery = context.query.search;
   let result: SearchResult[] = [];
   let errorMessage = null;
-  try{
-     result = (await performSearch(searchQuery)).data ?? [];
-  } 
-  catch(error){
+  try {
+    result = (await performSearch(searchQuery)).data ?? [];
+  }
+  catch (error) {
     errorMessage = "Opps!!! Something went wrong." + error
   }
-  
-  
+
+
   return {
-    props: {searchQuery, searchResults: result, errorMessage}, // will be passed to the page component as props
+    props: { searchQuery, searchResults: result, errorMessage }, // will be passed to the page component as props
   }
 }
